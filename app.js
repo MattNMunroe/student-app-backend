4e972c2889a5eaae57d848d51299f653597fee18
Matt M <Afrodesiacc@gmail.com>
2023-01-11 17:37:49 -0500

    Added necessary dependencies.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+//Dependencies  
 const express = require('express');
+const cors = require ("cors")
 
+//Config
 const app = express();
+
+//Mid-Ware
+app.use(cors());
 app.use(express.json());
 
+//Routes
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("Not found!")
 })
 
 module.exports = app
